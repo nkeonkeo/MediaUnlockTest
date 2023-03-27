1fd0f5e10f8e1fd65442a93396afcbf87cd44049
neko <nkeonkeo@outlook.com>
2023-03-27 16:52:43 +0800

    fixed bahamutAnime

--- BahamutAnime.go
+++ BahamutAnime.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"net/http/cookiejar"
 )
 
 func BahamutAnime(c http.Client) Result {
-	resp, err := GET(c, "https://ani.gamer.com.tw/ajax/token.php?adID=89422&sn=14667")
+	c.Jar, _ = cookiejar.New(nil)
+	resp, err := GET(c, "https://ani.gamer.com.tw/ajax/getdeviceid.php")
 	if err != nil {
-		return Result{Success: false, Err: err}
+		return Result{Success: false, Info: "Failed"}
 	}
 	defer resp.Body.Close()
 	b, err := io.ReadAll(resp.Body)
@@ -17,7 +19,20 @@ func BahamutAnime(c http.Client) Result {
 		return Result{Success: false, Err: err}
 	}
 	var res struct {
-		AnimeSn int
+		AnimeSn  int
+		Deviceid string
+	}
+	if err := json.Unmarshal(b, &res); err != nil {
+		return Result{Success: false}
+	}
+	resp, err = GET(c, "https://ani.gamer.com.tw/ajax/token.php?adID=89422&sn=14667&device="+res.Deviceid)
+	if err != nil {
+		return Result{Success: false, Err: err}
+	}
+	defer resp.Body.Close()
+	b, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return Result{Success: false, Err: err}
 	}
 	if err := json.Unmarshal(b, &res); err != nil {
 		return Result{Success: false}
